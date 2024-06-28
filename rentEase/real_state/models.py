d126f05e4b0858d5108be367ebefbb007f0d5031
Sebastian Trujillo <juanstt99@gmail.com>
2024-06-28 04:12:24 -0500

    feat(models): create Property Type model

--- rentEase/real_state/models.py
+++ rentEase/real_state/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 
+class PropertyType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
 class Property(models.Model):
     name = models.CharField(max_length=200)
-    type = models.CharField(max_length=50)
+    type = models.ForeignKey(PropertyType, on_delete=models.CASCADE)
     address = models.CharField(max_length=255)
     postal_code = models.CharField(max_length=20)
     city = models.CharField(max_length=100)
